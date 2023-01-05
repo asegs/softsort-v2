b59335b60644250f9de2ede200fe450fb918eca1
asegs <ajs2290@g.rit.edu>
2023-01-05 18:23:23 -0500

    Added cowboy first endpoint

--- src/softsort_app.erl
+++ src/softsort_app.erl
@@ -10,6 +10,14 @@
 -export([start/2, stop/1]).
 
 start(_StartType, _StartArgs) ->
+  Dispatch = cowboy_router:compile([
+    { <<"localhost">>, [{<<"/">>, api, []}] }
+  ]),
+  {ok, _} = cowboy:start_clear(
+    hello_handler,
+    [{port, 8080}],
+    #{env => #{dispatch => Dispatch}}
+  ),
   deriver:derive_schema_options("example"),
     softsort_sup:start_link().
 
